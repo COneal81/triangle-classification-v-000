4aeb505a443ed02e059d5f514d5de101729707f9
Kari O'Neal <triadfirewood@gmail.com>
2018-08-30 11:15:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,19 @@
 class Triangle 
   attr_accessor :side_1, :side_2, :side_3
   
-  def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(s1, s2, s3)
+    @triangle = []
+    @triangle << s1
+    @triangle << s2
+    @triangle << s3
   end  
   
-  
-  
+  def valid?
+    sum_s1_s2 = s1[0] + s2[1]
+    sum_s1_s3 = s1[0] + s3[2]
+    sum_s2_s3 = s2[1] + s3[2]
+    if 
+    
   
   def kind
     if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
